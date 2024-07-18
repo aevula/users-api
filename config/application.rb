06a2ef5745771244bd87fffc12181e984d708aca
Andzhey Evula <aevula@mail.ru>
2024-07-18 22:11:19 +0300

    Dedski-7 added top-level constant

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-::Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
